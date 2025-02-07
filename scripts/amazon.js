da38924e7d4219259a3ba72df702744fcef68984
theflyestnerd <kolaolari@gmail.com>
2025-02-07 19:12:56 +0100

    Make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -53,10 +53,8 @@ products.forEach((product) => {
         </div>
     `;
 })
-console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) =>{
     button.addEventListener('click', () =>{
@@ -76,7 +74,12 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity: 1
             });
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
 
