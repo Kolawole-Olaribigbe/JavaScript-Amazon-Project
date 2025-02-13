00c687737a03623b0e72583495bbe273352ec71a
theflyestnerd <kolaolari@gmail.com>
2025-02-13 19:25:38 +0100

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -21,6 +21,7 @@ export function addToCart(productId) {
             matchingItem = cartItem;
         }
     });
+
     if (matchingItem) {
         matchingItem.quantity += 1;
     } else {
@@ -42,5 +43,17 @@ export function removeFromCart(productId) {
         }
     })
     cart = newCart;
+    saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+    matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
