043661d7b754e24d228e9af88ca98bf7c4901c5c
theflyestnerd <kolaolari@gmail.com>
2025-02-26 20:30:04 +0100

    Converted from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,18 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+async function loadPage() {
+    await loadProductsFetch();
+    const value = await new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value3');
+        });
+    })
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve) => {
@@ -18,6 +30,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 /*
 new Promise((resolve) => {
     loadProducts(() => {
